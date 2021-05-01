a25e8dc504320dea5cbf058c94f5a9193f24bc75
VasaviLagishetty <vasavi2910@gmail.com>
2021-05-01 22:07:15 +0530

    Added each field as requied

--- MusicPlayer/models.py
+++ MusicPlayer/models.py
@@ -5,25 +5,25 @@ from django.utils import timezone
 # Create your models here.
 
 class Songs(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
-    duration = models.PositiveIntegerField()
+    id = models.IntegerField(primary_key=True, blank=False)
+    name = models.CharField(max_length=100, blank=False)
+    duration = models.PositiveIntegerField(blank=False)
     uploaded = models.DateTimeField(default = timezone.now)
 
 class Podcasts(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
-    duration = models.PositiveIntegerField()
+    id = models.IntegerField(primary_key=True, blank=False)
+    name = models.CharField(max_length=100, blank=False)
+    duration = models.PositiveIntegerField(blank=False)
     uploaded = models.DateTimeField(default = timezone.now)
-    host = models.CharField(max_length=100)
+    host = models.CharField(max_length=100,blank=False)
     participants = ArrayField(
             models.CharField(max_length=100), size = 10, blank = True, default = list
           )
 
 class AudioBook(models.Model):
-    id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    narrator = models.CharField(max_length=100)
-    duration = models.PositiveIntegerField()
+    id = models.IntegerField(primary_key=True, blank=False)
+    title = models.CharField(max_length=100,blank=False)
+    author = models.CharField(max_length=100, blank=False)
+    narrator = models.CharField(max_length=100, blank=False)
+    duration = models.PositiveIntegerField(blank=False)
     uploaded = models.DateTimeField(default = timezone.now)
